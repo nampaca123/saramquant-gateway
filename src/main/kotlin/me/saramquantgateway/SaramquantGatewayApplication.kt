676e1d25981aa93de705a4a563a78f25d91e64cc
Kellan Nam <nampaca123@gmail.com>
2026-02-21 23:05:27 +0900

    260221 jsonJackson

--- src/main/kotlin/me/saramquantgateway/SaramquantGatewayApplication.kt
+++ src/main/kotlin/me/saramquantgateway/SaramquantGatewayApplication.kt
@@ -1,14 +1,21 @@
 package me.saramquantgateway
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
 @EnableScheduling
-class SaramquantGatewayApplication
+class SaramquantGatewayApplication {
+
+    @Bean
+    fun objectMapper(): ObjectMapper = jacksonObjectMapper()
+}
 
 fun main(args: Array<String>) {
     runApplication<SaramquantGatewayApplication>(*args)
